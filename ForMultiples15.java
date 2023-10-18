662ff0ec9a9eb4269481ea08136c00568269eca1
CipherPixy032 <mollym3gtr@gmail.com>
2023-10-18 13:22:05 +0700

    Submit Tugas Experiment 1 No.3

--- ForMultiples15.java
+++ ForMultiples15.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class ForMultiples15 {
     public static void main(String[] args) {
-        Scanner input15 = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         int multiple = 15;
         int sum = 0;
         int counter = 0;
 
         System.out.println("Input the multiple: ");
-        multiple = input15.nextInt();
+        multiple = input.nextInt();
 
         for (int i = 1; i <= 50; i++) {
             if (i % multiple == 0) {
@@ -18,8 +18,10 @@ public class ForMultiples15 {
             }
         }
 
-        System.out.printf("There are %d numbers that are multiple of %d in range 1 to 50. \n", counter, multiple);
-        System.out.printf("The sum from all multiples of %d in range 1 s.d. 50 is %d. \n", multiple, sum);
+        double average = (double) sum / counter; // Calculate the average as a double
+
+        System.out.printf("There are %d numbers that are multiples of %d in the range 1 to 50.\n", counter, multiple);
+        System.out.printf("The sum of all multiples of %d in the range 1 to 50 is %d.\n", multiple, sum);
+        System.out.printf("The average of all multiples of %d in the range 1 to 50 is %.2f.\n", multiple, average);
     }
 }
-
