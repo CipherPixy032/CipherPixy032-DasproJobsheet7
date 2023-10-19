ee1fce05e543efaa1c9ccb2fd1988ddebb58d336
CipherPixy032 <mollym3gtr@gmail.com>
2023-10-20 00:15:50 +0700

    Submit Tugas Experiment 3

---
+++ DoWhileLeaveEntitlement15.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DoWhileLeaveEntitlement15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int leaveEntitlement, numLeave;
+        String confirmation;
+
+        System.out.print("Number of leave entitlement: ");
+        leaveEntitlement = input.nextInt();
+
+        do {
+            System.out.print("Do you want to take leave (y/n)? ");
+            confirmation = input.next();
+
+            if (confirmation.equalsIgnoreCase("y")) {
+                System.out.print("Leave number: ");
+                numLeave = input.nextInt();
+
+                if (numLeave <= leaveEntitlement) {
+                    leaveEntitlement -= numLeave;
+                    System.out.println("Remaining leave entitlement: " + leaveEntitlement);
+
+
+                } else {
+                    System.out.println("The remaining leave entitlement is not sufficient.");
+                    break;
+                }
+            }
+        } while (leaveEntitlement > 0);
+    }
+}
